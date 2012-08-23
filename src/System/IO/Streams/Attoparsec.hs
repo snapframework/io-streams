19c3621f5e4b7ba2f38a15a1a0e3247b309a98b2
Gregory Collins <greg@gregorycollins.net>
2012-08-23 23:16:26 +0200

    Remove parseFromStreamWithLeftovers, it's no longer necessary

--- src/System/IO/Streams/Attoparsec.hs
+++ src/System/IO/Streams/Attoparsec.hs
@@ -1,23 +1,16 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE OverloadedStrings  #-}
-module System.IO.Streams.Attoparsec
-  ( ParseException(..)
-  , parseFromStream
-  , parseFromStreamWithLeftovers
-  )
-where
+module System.IO.Streams.Attoparsec where
 
 ------------------------------------------------------------------------------
-import           Control.Exception
-import           Control.Monad                    (when)
-import           Data.Attoparsec.ByteString.Char8
-import           Data.ByteString.Char8            (ByteString)
-import qualified Data.ByteString.Char8            as S
-import           Data.Maybe
-import           Data.Typeable
-import           Prelude                          hiding (read)
+import Control.Exception
+import Data.Attoparsec.ByteString.Char8
+import Data.ByteString.Char8            (ByteString)
+import Data.Maybe
+import Data.Typeable
+import Prelude                          hiding (read)
 ------------------------------------------------------------------------------
-import           System.IO.Streams.Internal
+import System.IO.Streams.Internal
 
 ------------------------------------------------------------------------------
 data ParseException = ParseException String
@@ -30,31 +23,23 @@ instance Exception ParseException
 
 
 ------------------------------------------------------------------------------
-parseFromStreamWithLeftovers :: Parser r
-                             -> InputStream ByteString
-                             -> IO (Either String r, ByteString)
-parseFromStreamWithLeftovers parser is = do
+parseFromStream :: Parser r
+                -> InputStream ByteString
+                -> IO r
+parseFromStream parser is = do
     read is >>= maybe (finish $ parse parser "")
                       (go . parse parser)
 
   where
     finish k = let k' = feed (feed k "") ""
                in case k' of
-                    Fail x _ _ -> return (err k' , x )
-                    Partial _  -> return (err k' , "")
-                    Done x r   -> return (Right r, x )
+                    Fail x _ _ -> unRead x is >> err k'
+                    Partial _  -> err k'
+                    Done x r   -> unRead x is >> return r
 
-    err r = eitherResult r
 
-    go r@(Fail x _ _) = return (err r, x)
-    go (Done x r)     = return (Right r, x)
+    err r = let (Left s) = eitherResult r in throwIO $ ParseException s
+
+    go r@(Fail x _ _) = unRead x is >> err r
+    go (Done x r)     = unRead x is >> return r
     go r              = read is >>= maybe (finish r) (go . feed r)
-
-
-parseFromStream :: Parser r
-                -> InputStream ByteString
-                -> IO r
-parseFromStream parser is = do
-    (e, l) <- parseFromStreamWithLeftovers parser is
-    when (not $ S.null l) $ unRead l is
-    either (throwIO . ParseException) return e
