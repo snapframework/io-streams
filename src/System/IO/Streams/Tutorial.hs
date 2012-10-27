4d7aa81b76c5ad820851a8797c0b16ebf1d792a6
Gabriel Gonzalez <Gabriel439@gmail.com>
2012-10-26 20:24:21 -0700

    Updated tutorial with Greg's suggestions

--- src/System/IO/Streams/Tutorial.hs
+++ src/System/IO/Streams/Tutorial.hs
@@ -18,35 +18,57 @@ import System.IO (Handle)
     processing:
 
     * 'InputStream': a read-only smart handle
+
     * 'OutputStream': a write-only smart handle
 
     The 'InputStream' type implements all the core operations we expect for a
-    read-only handle.  We can consume values using 'read':
+    read-only handle.  We consume values using 'read', which returns a 'Nothing'
+    when the resource is done:
+    {- NOTE: I'm not sure if I should use the term EOF since it might confuse
+             the reader to think these are only for files -}
 
-    {- NOTE: This doesn't explain why these use 'Maybe'.  I don't know how to
-       work that in here without disrupting the flow.  Perhaps put it off to
-       a later section. -}
 > read :: InputStream c -> IO (Maybe c)
 
-    ... push back values using 'unRead':
+    ... and we can push back values using 'unRead':
 
 > unRead :: c -> InputStream c -> IO ()
 
-    {- NOTE: Maybe skip mentioning 'peek' to emphasize simplicity -}
-    ... or 'peek' at the next available value without consuming it:
-
-> peek :: InputStream c -> IO (Maybe c)
-
     The 'OutputStream' type implements the 'write' operation which feeds it
-    output:
+    output, supplying 'Nothing' to signal resource exhaustion:
 
 > write :: Maybe c -> OutputStream c -> IO ()
 
-    You could use these types as drop-in replacements for any handle-based API,
-    but they differ from ordinary handles by supporting a wide variety
-    of stream transformations that don't perturb the user-facing API.
-    Consequently, users get the best of both worlds: the simplicity and
-    convention of handles with the power and flexibility of stream processing.
+    These streams slightly resemble Haskell 'Handle's, but support a wider
+    range of sources and sinks.  For example, you can convert an ordinary list
+    to an 'InputStream' source and interact with it using the handle-based API:
+
+>>> import qualified System.IO.Streams as S
+>>> listHandle <- S.fromList [1, 2]
+>>> read listHandle
+Just 1
+>>> read listHandle
+Just 2
+>>> read listHandle
+Nothing
+
+    Additionally, IO streams come with a library of stream transformations that
+    preserve their handle-like API.  For example, you can map a function over an
+    'InputStream', which generates a new handle to the same stream that returns
+    transformed values:
+
+>>> oldHandle <- S.fromList [1, 2, 3]
+>>> newHandle <- S.mapM (* 10) oldHandle
+>>> read newHandle
+10
+>>> -- We can still view the stream through the old handle
+>>> read oldHandle
+2
+>>> -- ... and switch back again
+>>> read newHandle
+30
+
+    IO streams focus on preserving the convention of traditional handles while
+    offering a wider library of stream-processing utilities.
 -}
 
 {- $create
@@ -54,64 +76,56 @@ import System.IO (Handle)
     The @io-streams@ library provides a simple interface for creating your
     own 'InputStream's and 'OutputStream's.
 
-    You can build any 'InputStream' from an 'IO' action that generates output,
+    You can build an 'InputStream' from any 'IO' action that generates output,
     as long as it wraps results in 'Just' and uses 'Nothing' to signal EOF:
 
 > makeInputStream :: IO (Maybe a) -> IO (InputStream a)
 
     Similarly, you can build any 'OutputStream' from an 'IO' action that accepts
-    input, as long as it accepts 'Just' for more input and 'Nothing' for EOF:
+    input, as long as it interprets 'Just' as more input and 'Nothing' as EOF:
 
 > makeOutputStream :: (Maybe a -> IO ()) -> IO (OutputStream a)
 
     As an example, let's wrap an ordinary read-only 'Handle' in an
     'InputStream':
 
-    {- NOTE: Perhaps I should match the definition in System.IO.Streams.Handle
-             since it's pretty simple already and it would impress the reader
-             to later discover that the library's own implementation was in
-             fact that simple. -}
-
-> import Data.ByteString (ByteString, hGet)
-> import System.IO.Streams (InputStream, OutputStream)
+> import Data.ByteString (ByteString)
+> import qualified Data.ByteString as S
+> import System.IO.Streams (InputStream)
 > import qualified System.IO.Streams as Streams
-> import System.IO (Handle, hIsEOF)
+> import System.IO (Handle, hFlush)
+>
+> bUFSIZ = 32752
 >
 > upgradeReadOnlyHandle :: Handle -> IO (InputStream ByteString)
-> upgradeReadOnlyHandle h = Streams.makeInputStream $ do
->     eof <- hIsEOF h
->     if eof
->     then return Nothing
->     else do
->         bytes <- hGet h 4096
->         return (Just bytes)
+> upgradeReadOnlyHandle h = Streams.makeInputStream f
+>   where
+>     f = do
+>         x <- S.hGetSome h BUFSIZ
+>         return $! if S.null x then Nothing else Just x
 
     Now that we've upgraded it, we can tap into all the stream-processing
     features this library provides.  For example, we can decompress the input:
 
-    {- NOTE: Let me know if you prefer ordinary do notation instead of (>=>) -}
-
-> import System.IO.Streams.Zlib
->
 > unzipHandle :: Handle -> IO (InputStream ByteString)
-> unzipHandle = upgradeReadOnlyHandle >=> decompress
+> unzipHandle = upgradeReadOnlyHandle >=> Streams.decompress
 
     ... or we could prevent a denial-of-service attack:
 
-> import System.IO.Streams.ByteString
->
 > protectHandle :: Handle -> IO (InputStream ByteString)
-> protectHandle = upgradeReadOnlyHandle >=> throwIfProducesMoreThan 1000000
+> protectHandle =
+>     upgradeReadOnlyHandle >=> Streams.throwIfProducesMoreThan 1000000
 
     We don't even really need to write the @upgradeReadOnlyHandle@ function,
-    because this library already provides one:
+    because this library already provides one that uses the same implementation
+    given above:
 
 > -- from System.IO.Streams.Handle
 > handleToInputStream :: Handle -> IO (InputStream ByteString)
 
-    We see a pattern: @io-streams@ includes a complete library of standard
-    functions that anticipates our needs.  You automatically tap into this
-    suite of tools whenever you upgrade your resources to IO streams.
+    @io-streams@ provides many useful functions such as these in its standard
+    library and you can take advantage of them by transforming your resources
+    into IO streams.
 -}
 
 {- TODO:
