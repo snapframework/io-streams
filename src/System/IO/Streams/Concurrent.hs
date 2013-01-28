cb37c39e127f5095d7405e1ff4486bb1905623c4
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-01-28 11:23:50 -0800

    Added concurrentMerge to System.IO.Streams.Concurrent

--- src/System/IO/Streams/Concurrent.hs
+++ src/System/IO/Streams/Concurrent.hs
@@ -7,16 +7,22 @@ module System.IO.Streams.Concurrent
    inputToChan
  , chanToInput
  , chanToOutput
+ , concurrentMerge
  ) where
 
 ------------------------------------------------------------------------------
+import Control.Concurrent         ( forkIO )
 import Control.Concurrent.Chan    ( Chan, readChan, writeChan )
+import Control.Concurrent.MVar    ( newEmptyMVar, putMVar, takeMVar )
+import Control.Exception          ( mask, try, throwIO, SomeException)
+import Control.Monad              ( forM_ )
 import Data.Maybe                 ( isNothing )
 import Prelude             hiding ( read )
 ------------------------------------------------------------------------------
 import System.IO.Streams.Internal ( InputStream
                                   , OutputStream
                                   , SP (..)
+                                  , makeInputStream
                                   , makeOutputStream
                                   , nullSource
                                   , sourceToStream
@@ -53,3 +59,33 @@ chanToInput ch = sourceToStream src
 --
 chanToOutput :: Chan (Maybe a) -> IO (OutputStream a)
 chanToOutput = makeOutputStream . writeChan
+
+
+------------------------------------------------------------------------------
+-- | Concurrently merges two 'InputStream's combining values in the order they
+-- become available
+--
+-- Does /not/ forward end-of-stream notifications
+--
+-- This traps exceptions in each concurrent thread and re-raises them in the
+-- current thread
+concurrentMerge :: [InputStream a] -> IO (InputStream a)
+concurrentMerge iss = do
+    mv <- newEmptyMVar
+    mask $ \restore -> forM_ iss $ \is -> forkIO $ do
+        let producer = do
+                emb <- try $ restore $ read is
+                case emb of
+                    Left exc       -> do
+                        putMVar mv (Left (exc :: SomeException))
+                        producer
+                    Right (Just a) -> do
+                        putMVar mv (Right a)
+                        producer
+                    Right Nothing  -> return ()
+        producer
+    makeInputStream $ do
+        emb <- takeMVar mv
+        case emb of
+            Left exc -> throwIO exc
+            Right a  -> return (Just a)
