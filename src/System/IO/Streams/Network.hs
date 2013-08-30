ce3a6b5eb4953dd20728a8a10f27e693f684aa7c
Gregory Collins <greg@gregorycollins.net>
2013-08-30 17:30:20 +0200

    Network: add control over buffer size to socketToStreams.

--- src/System/IO/Streams/Network.hs
+++ src/System/IO/Streams/Network.hs
@@ -2,15 +2,24 @@
 module System.IO.Streams.Network
   ( -- * Sockets to Streams
     socketToStreams
+  , socketToStreamsWithBufferSize
   ) where
 
 ------------------------------------------------------------------------------
 import           Data.ByteString.Char8      (ByteString)
 import qualified Data.ByteString.Char8      as S
+import           Foreign.Storable           (sizeOf)
 import           Network.Socket             (Socket)
 import qualified Network.Socket.ByteString  as N
 import           System.IO.Streams.Internal (InputStream, OutputStream)
 import qualified System.IO.Streams.Internal as Streams
+
+
+------------------------------------------------------------------------------
+bUFSIZ :: Int
+bUFSIZ = 8192 - overhead
+  where
+    overhead = 4 * sizeOf (undefined :: Int)
 
 
 ------------------------------------------------------------------------------
@@ -19,16 +28,25 @@ import qualified System.IO.Streams.Internal as Streams
 -- 'OutputStream' does not cause the underlying 'Socket' to be closed.
 socketToStreams :: Socket
                 -> IO (InputStream ByteString, OutputStream ByteString)
-socketToStreams socket = do
+socketToStreams = socketToStreamsWithBufferSize bUFSIZ
+
+
+------------------------------------------------------------------------------
+-- | Converts a 'Socket' to an 'InputStream' \/ 'OutputStream' pair. Note that,
+-- as is usually the case in @io-streams@, writing a 'Nothing' to the generated
+-- 'OutputStream' does not cause the underlying 'Socket' to be closed.
+socketToStreamsWithBufferSize
+    :: Int                      -- ^ how large the receive buffer should be
+    -> Socket                   -- ^ network socket
+    -> IO (InputStream ByteString, OutputStream ByteString)
+socketToStreamsWithBufferSize bufsiz socket = do
     is <- Streams.makeInputStream input
     os <- Streams.makeOutputStream output
     return $! (is, os)
 
   where
-    bUFSIZ = 32752
-
     input = do
-        s <- N.recv socket bUFSIZ
+        s <- N.recv socket bufsiz
         return $! if S.null s then Nothing else Just s
 
     output Nothing  = return $! ()
